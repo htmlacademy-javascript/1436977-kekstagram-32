2d9b33f232f620f28a89ca9b1342b60cc5e871a8
avantell <116308241+avantell@users.noreply.github.com>
2024-07-13 08:41:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,66 @@
-const pictureCount = 25;
-const minLike = 15
-const maxLike = 200
-const minCommentCount = 0
-const maxCommentCount = 30
-const minAvatarCount = 1
-const maxAvatarCount = 6
-const names = ['Илья', 'Таня', 'Настя', 'Наташа', 'Костя']
+const name = ['Илья', 'Таня', 'Настя', 'Наташа', 'Костя'];
 
-let commentLines = [
+const getRandomComment = [
   'Всё отлично!, В целом всё неплохо, Но не всё', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра', 'В конце концов это просто непрофессионально', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
 
-const description = [
+const letDescription = [
   'работаю дома', 'чилю на диване', 'Катаюсь на роликах', 'занимаюсь в тренажерном зале'
 ];
 
-const getRandomArrayElement = (items) =>
-items [getRandomInteger(0, items.length - 1)]
 
+/**
+ * Создаем объект с нужными ключами
+ */
 
-
-const getPictureCount = Array.from({length: picterCount}, (element, index) => creatPicters(index + 1));
-
-const createGenerator = () => {
-
-  let number = 0
-  return () => {
-    numberId += 1
-    return numberId
-  }
-}
-
-const getRandomInteger = (a, b) => {
-
-  const lower = Math.ceil(Math.min(a, b))
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-}
-
-const generateRandomId = createGenerator()
-
-const createMessage = () => Array.from({length: getRandomInteger (1, 2)}, () => getRandomArrayElement(commentLines),)
-
-const createComment = () => (
-{
-  id: generateRandomId(),
-  avatar: `img/avatar-${getRandomInteger(minAvatarCount, maxAvatarCount)}.svg`,
-  message: 'В целом всё неплохо. Но не всё.',
-  name: getRandomArrayElement (names),
-})
-
-const creatPicture = (index) => ({
-  id: `photos/${{index}}.jpg`,
-  url: '',
-  description: getRandomArrayElement (description),
-  likes: getRandomInteger (minLike, maxLike),
-  comments: Array.from({length: getRandomInteger (minCommentCount, maxCommentCount)}, createComment)
+const makeObject = (idx) => ({
+  id: getRandomId(),
+  url: getRandomUrl(),
+  description: 'ad',
+  likes: 213,
+  comments: 23
 });
 
-const getPictures = () => Array.from(
-  {length: pictureCount},
-  (_, index) => creatPicture(index + 1)
-)
+makeObject();
 
-getPictures()
+/**
+ * Данная функция создает массив объектов в кол-ве 25шт
+ */
+const makePhotos = (count) => {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result[i] = makeObject(i + 1);
+  }
+  return result;
+};
+makePhotos(25);
+
+/**
+ * Создаем нужное число id в рандомном порядке
+ */
+
+function getRandomId(min, max) {
+  const minId = Math.ceil(min);
+  const maxId = Math.floor(max);
+  return Math.floor(Math.random() * (maxId - minId) + minId); // The maximum is exclusive and the minimum is inclusive
+}
+getRandomId (1, 25);
+
+/**
+ * Создаем нужное число url в рандомном порядке
+ */
+function getRandomUrl(min, max) {
+  const minUrl = Math.ceil(min);
+  const maxUrl = Math.floor(max);
+  return Math.floor(Math.random() * (maxUrl - minUrl) + minUrl);
+}
+getRandomUrl (1, 25);
+
+// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+
+// const createComment = () => (
+//   {
+//     id: generateRandomId(),
+//     avatar: img/avatar-${getRandomInteger(minAvatarCount, maxAvatarCount)}.svg,
+//     message: 'В целом всё неплохо. Но не всё.',
+//     name: getRandomArrayElement (names),
+//   })
