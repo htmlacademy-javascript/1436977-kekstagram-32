4b4d70b39e9ae887d65904ea29489445cdde94f5
Keks <keks@htmlacademy.ru>
2024-07-22 12:29:43 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,5 @@
+import {generatePhotos} from './data.js';
+
+const photoArray = generatePhotos(); // Генерируем массив фотографий.
+console.log(photoArray); // Выводим массив фотографий в консоль.
+
---
+++ 7/js/util.js
@@ -0,0 +1,12 @@
+export {getRandomInt, getRandomElement} from './util.js'; //Экспортируем функции в файл util.js
+
+// Функция getRandomInt(min, max) возвращает случайное целое число в диапазоне от min до max включительно.
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Функция getRandomElement(arr) возвращает случайный элемент из массива arr.
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
