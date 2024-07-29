6ba461e639439d5e5c64d3203c14bec0167a8ca3
avantel <dev.avantel@gmail.com>
2024-07-29 19:16:46 +0300

    Создал отдельный модуль thumbnail.js. В нем создал функции и по генерации шаблонов и выводу картинок.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {generatePhotos} from './data.js';
+import {generateThumbnails} from './thumbnail.js';
 
+generateThumbnails(generatePhotos())
 const photoArray = generatePhotos(); // Генерируем массив фотографий.
 console.log(photoArray); // Выводим массив фотографий в консоль.
 
